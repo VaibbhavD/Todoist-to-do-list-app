33070adee89b8d421ae0db4c82c9246c963b21fe
VaibhavDhamanage <vaibhavdhamanage>
2023-12-13 22:06:27 +0530

    data Store for temprory and fix still not add or remove function willrun.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const apiid="33565f3feefb484499b59440112e2b0c";
+const reman="aa6f99e331dd4c2d87de168bb7278f80";
+const don="4cd78f95e55f4c48a41b4cb54299ba19";
 
 
 function savedetails(event){
@@ -10,28 +11,34 @@ function savedetails(event){
        name,
        description,
    }
-
-   
+   axios.post("https://crudcrud.com/api/"+reman+"/reaminingtodo",todoobj)
+   .then(res=>console.log(res))
+   .catch(err=>console.log(err))
 
    showitemdetails(todoobj);
+
    document.getElementById("name").value="";
    document.getElementById("description").value="";
-
 }
 
+// ---------------------------------------------------
 
+    window.addEventListener("DOMContentLoaded",()=>{
+        axios.get("https://crudcrud.com/api/"+reman+"/reaminingtodo")
+        .then(response => {
+            for(var i=0;i<response.data.length;i++)
+            {
+                if(response.data.status==404)
+                {
+                    continue;
+                }
+                showitemdetails(response.data[i]);
+            }
+        })
+        .catch(err=>console.log(err))
+        })
 
-   window.addEventListener("DOMContentLoaded",()=>{
-    axios.get("https://crudcrud.com/api/"+apiid+"/inventorystocks")
-    .then(response => {
-        for(var i=0;i<response.data.length;i++)
-        {
-            showsavedtodo(response.data[i]);
-        }
-    })
-    .catch(err=>console.log(err))
-    })
-
+// =---------------------------------------------
 
 function showitemdetails (todoobj){
     itemobj={
@@ -40,7 +47,6 @@ function showitemdetails (todoobj){
         description:todoobj.description,
     }
    const parent=document.getElementById("remaing");
-   const parent2=document.getElementById("done");
    const child=document.createElement("li");
 
    const pname=document.createElement("ol");
@@ -54,34 +60,53 @@ function showitemdetails (todoobj){
    child.appendChild(pdes);
    parent.appendChild(child);
    
+   
 
    const donee=document.createElement('input')
    donee.type="button";
    donee.className="donee";
    donee.value="DONE";
    donee.onclick=()=>{
-    
-    parent.removeChild(child);
-    parent2.appendChild(child);
-    axios.post("https://crudcrud.com/api/"+apiid+"/inventorystocks",todoobj)
-   .then(res=>console.log(res))
-   .catch(err=>console.log(err))
+    var id=todoobj._id;
+       savedtodo(todoobj); 
+       del(id);
+       parent.removeChild(child);
    }
    child.appendChild(donee);
 
+    //    ------------------------------------------------------   
    const remov=document.createElement('input')
    remov.type="button";
    remov.className="remov";
    remov.value="REMOVE";
    remov.onclick=()=>{
     parent.removeChild(child);
-    axios.delete("https://crudcrud.com/api/"+apiid+"/inventorystocks/"+todoobj._id)
+    axios.delete("https://crudcrud.com/api/"+reman+"/reaminingtodo/"+itemobj._id)
     .then(res=>console.log(res))
-    .then(err=>console.log(err));
+    .catch(err=>console.log(err));
    }
    child.appendChild(remov);
+}
 
+// --------------------------------------------
 
+function savedtodo (todoobj){
+    itemobj={
+        name:todoobj.name,
+        description:todoobj.description,
+    }
+    var id=todoobj._id;
+    axios.post("https://crudcrud.com/api/"+don+"/done",itemobj)
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err))
+    showsavedtodo(todoobj);
+}
+
+function del(id){
+    axios.delete("https://crudcrud.com/api/"+reman+"/reaminingtodo/"+id)
+    .then(res=>console.log(res))
+    .catch(err=>console.log(err));
+    console.log(itemobj._id);
 }
 
 function showsavedtodo (todoobj){
@@ -90,6 +115,7 @@ function showsavedtodo (todoobj){
         name:todoobj.name,
         description:todoobj.description,
     }
+   
    const parent2=document.getElementById("done");
    const child=document.createElement("li");
 
@@ -103,5 +129,18 @@ function showsavedtodo (todoobj){
    child.appendChild(pname);
    child.appendChild(pdes);
    parent2.appendChild(child);
-   
 }
+
+// ------------------------------------------------------------------
+
+window.addEventListener("DOMContentLoaded",()=>{
+    axios.get("https://crudcrud.com/api/"+don+"/done")
+    .then(response => {
+        for(var i=0;i<response.data.length;i++)
+        {
+
+            showsavedtodo(response.data[i]);
+        }
+    })
+    .catch(err=>console.log(err))
+    })
